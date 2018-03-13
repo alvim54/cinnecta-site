4811b8dcf82acd82f8c1de7f71cf4b3649ea1d4e
Álvaro Borges <alvaroborges@MacBook-Pro-de-Alvaro-2.local>
2018-03-13 13:32:48 -0300

    Configuração do form do whitepaper

--- src/components/WhitePaperForm.js
+++ src/components/WhitePaperForm.js
@@ -3,23 +3,24 @@ import React from 'react';
 const WhitePaperForm = () => (
 	<section className="white-papper-form">
 		<div className="grid-container text-center">
-			<form>
+			<form name="whitepaper" method="post" data-netlify="true" action="/success" data-netlify-honeypot="bot-field">
+				<input type="hidden" name="form-name" value="whitepaper" />
 				<div className="grid-x grid-padding-x">
 					<div className="small-12 cell">
 						<h3>Quer saber mais?</h3>
 						<p>Preencha o formulário e baixe nosso material</p>
 					</div>
 					<div className="small-12 medium-6 cell">
-						<input type="text" placeholder="Nome" />
+						<input type="text" name="nome" placeholder="Nome" />
 					</div>
 					<div className="small-12 medium-6 cell">
-						<input type="text" placeholder="Empresa" />
+						<input type="text" name="empresa" placeholder="Empresa" />
 					</div>
 					<div className="small-12 medium-6 cell">
-						<input type="text" placeholder="E-mail" />
+						<input type="text" name="email" placeholder="E-mail" />
 					</div>
 					<div className="small-12 medium-6 cell">
-						<input type="text" placeholder="Telefone" />
+						<input type="text" name="telefone" placeholder="Telefone" />
 					</div>
 				</div>
 				<button type="submit" className="call-to-action button">Quero receber material exclusivo</button>
