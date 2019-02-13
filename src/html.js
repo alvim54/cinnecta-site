00397a4fe856bfc733cb2591b6fe29997133572d
Álvaro Borges <alvaro.borges@cinnecta.com>
2019-02-13 19:58:18 -0200

    share image

--- src/html.js
+++ src/html.js
@@ -47,7 +47,7 @@ class Html extends Component {
           />
           <link rel="manifest" href="/manifest.json" />
           <meta property="og:title" content="Cinnecta" />
-          <meta property="og:image" content="https://image.ibb.co/hEb18d/sn_image.jpg" />
+          <meta property="og:image" content="https://i.ibb.co/L930dvP/icon.png" />
           <meta property="og:type" content="company" />
           <meta property="og:description" content="A Cinnecta é uma empresa de tecnologia especializada em Gerenciamento de Comportamento do Usuário para canais digitais móveis. Fornecemos soluções inovadoras que permitem às empresas ter visibilidade sobre o Perfil de Uso e Experiência do seus clientes por meio do monitoramento do uso de smartphones, proporcionando o aumento do Engajamento e da Retenção." />
           <title>Cinnecta</title>
